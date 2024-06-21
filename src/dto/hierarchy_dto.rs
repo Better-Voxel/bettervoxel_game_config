124490018ebaff992dfbe4f650943182502964df
Thomas <thomas.prudhomme@epitech.eu>
2024-06-21 14:07:41 +0900

    [+] Add player prefab DTO

--- src/dto/hierarchy_dto.rs
+++ src/dto/hierarchy_dto.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use bevy_math::Vec3;
 use bevy_render::color::Color;
 
 use bevy_transform::prelude::Transform;
@@ -21,6 +22,7 @@ pub enum GameElementTypeDTO {
     Part(PartDTO),
     Folder(FolderDTO),
     Script(ScriptDTO),
+    PlayerPrefab(PlayerPrefabDTO)
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -39,12 +41,22 @@ pub struct ScriptDTO {
     pub script: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+#[cfg_attr(test, derive(PartialEq))]
+pub struct PlayerPrefabDTO {
+    pub position: Vec3,
+    pub hauteur: f32,
+    pub rayon: f32,
+    pub camera_offset: Vec3,
+    pub camera_look_at: Vec3,
+}
+
 #[cfg(test)]
 mod tests {
     use bevy_math::{Quat, Vec3};
     use bevy_render::color::Color;
     use bevy_transform::prelude::Transform;
-    use crate::dto::hierarchy_dto::PartDTO;
+    use crate::dto::hierarchy_dto::{PartDTO, PlayerPrefabDTO};
 
     const PART: PartDTO = PartDTO {
         transform: Transform {
@@ -74,5 +86,39 @@ mod tests {
     fn test_deserialize() {
         let part: PartDTO = serde_json::from_str(PART_JSON).unwrap();
         assert_eq!(PART, part);
+    }
+
+    const PLAYER: PlayerPrefabDTO = PlayerPrefabDTO {
+        position: Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        },
+        hauteur: 1.8,
+        rayon: 1.0,
+        camera_offset: Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        },
+        camera_look_at: Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        },
+    };
+
+    const PLAYER_JSON: & str = r#"{"position":[0.0,0.0,0.0],"hauteur":1.8,"rayon":1.0,"camera_offset":[0.0,0.0,0.0],"camera_look_at":[0.0,0.0,0.0]}"#;
+    #[test]
+    fn test_serialize_player_prefab() {
+        let player_json = serde_json::to_string(&PLAYER).unwrap();
+        println!("{player_json}");
+        assert_eq!(PLAYER_JSON, player_json);
+    }
+
+    #[test]
+    fn test_deserialize_player_prefab() {
+        let player = serde_json::from_str(&PLAYER_JSON).unwrap();
+        assert_eq!(PLAYER, player);
     }
 }
